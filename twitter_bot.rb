0ca7bd486554c9bcfc9270513194c08c053b472b
kkabetani <k.kabetani@gmail.com>
2014-06-08 02:07:49 +0900

    add bot main logic

---
+++ twitter_bot.rb
@@ -0,0 +1,11 @@
+require 'twitter'
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key       = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret    = ENV['TWITTER_CONSUMER_SECRET']
+  config.oauth_token        = ENV['TWITTER_ACCESS_TOKEN']
+  config.oauth_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+end
+
+now = Time.now
+client.update(now)
